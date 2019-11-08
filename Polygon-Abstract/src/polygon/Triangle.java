57530ad005ac2bce0d2f7e1f530a01bd78ef1bf4
Ryan <junhosong@stmarksschool.org>
2019-11-08 09:10:58 -0500

    Resolved comments
    11/9/19

--- Polygon-Abstract/src/polygon/Triangle.java
+++ Polygon-Abstract/src/polygon/Triangle.java
@@ -8,8 +8,9 @@ public class Triangle extends Polygon{
 	
 	public static final int NUMOFSIDES = 3;
 	
-	
-	
+	public Triangle() {
+		super();
+	}
 	
 	public Triangle(ArrayList<Point> coors) {
 		super(NUMOFSIDES, coors);
@@ -30,15 +31,15 @@ public class Triangle extends Polygon{
 		// TODO Auto-generated method stub
 		
 		Point a = coordinates.get(0);
-		
 		Point b = coordinates.get(1);
-		
 		Point c = coordinates.get(2);
-		
 		double crossP = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
-		
 		return Math.abs(crossP / 2);
 		
+	}
+	
+	public double findLength(Point a, Point b) {
+		return Math.round(Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)));
 	}
 
 	@Override
@@ -46,23 +47,14 @@ public class Triangle extends Polygon{
 		// TODO Auto-generated method stub
 		
 		Point a = coordinates.get(0);
-		
 		Point b = coordinates.get(1);
-		
 		Point c = coordinates.get(2);
 		
-		double length1 = Math.round(Math.sqrt((c.x - b.x) * (c.x - b.x) + (c.y - b.y) * (c.y - b.y)));
-		
-		double length2 = Math.round(Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)));
-		
-		double length3 = Math.round(Math.sqrt((c.x - a.x) * (c.x - a.x) + (c.y - a.y) * (c.y - a.y)));
-		
-		if(length1 == length2 && length3 == length2) {
+		double length1 = findLength(c,b);
+		double length2 = findLength(a,b);
+		double length3 = findLength(a,c);
 			
-			return true;
-			
-		}
-		return false;
+		return length1 == length2 && length3 == length2;		
 		
 		
 		
